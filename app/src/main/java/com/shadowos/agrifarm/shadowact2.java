27ce021666152fcb12a708bbcf131e2a16e2a983
theglitchh <souravgope765@gmail.com>
2020-05-21 18:51:24 +0530

    Agrifarm: Add new activity for Webview of covid-19
    -Agrifarm: Add new activity for webview of krishijagran

--- app/src/main/java/com/shadowos/agrifarm/shadowact2.java
+++ app/src/main/java/com/shadowos/agrifarm/shadowact2.java
@@ -5,17 +5,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class shadowact2 extends AppCompatActivity {
-
+private WebView webView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(com.shadowos.agrifarm.R.layout.activity_shadowact2);
+        webView = (WebView) findViewById(R.id.wewewew);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://www.covid19india.org/");
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+
+
     }
 
-    public void stylelame1(View view){
-        Intent intent = new Intent(this,shadowact3.class);
+
+    public void farmnews(View view) {
+        Intent intent = new Intent(this, shadowact4.class);
         startActivity(intent);
     }
+
 }
--- app/src/main/java/com/shadowos/agrifarm/shadowact4.java
+++ app/src/main/java/com/shadowos/agrifarm/shadowact4.java
@@ -1,13 +1,32 @@
 package com.shadowos.agrifarm;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class shadowact4 extends AppCompatActivity {
-
+    private WebView webView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shadowact4);
-    }
-}
+        webView = (WebView) findViewById(R.id.farmer);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://krishijagran.com/");
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.getSettings().setBuiltInZoomControls(true);
+
+    }    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+
+    }}
+

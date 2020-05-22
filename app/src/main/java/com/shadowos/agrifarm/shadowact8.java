f55b30353a5c27f897779e07c8ae7c0421dd00e5
theglitchh <souravgope765@gmail.com>
2020-05-23 02:04:26 +0530

    Agrifarm: Add new activity for SourceCode

---
+++ app/src/main/java/com/shadowos/agrifarm/shadowact8.java
@@ -0,0 +1,13 @@
+package com.shadowos.agrifarm;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class shadowact8 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shadowact8);
+    }
+}

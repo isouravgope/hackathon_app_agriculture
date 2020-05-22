e2aec89bed0244cec7d890a5d6575bd57775a9a0
theglitchh <souravgope765@gmail.com>
2020-05-23 02:14:54 +0530

    Agrifarm: Add new Intent function to call sourcecode WebView

--- app/src/main/java/com/shadowos/agrifarm/shadowact2.java
+++ app/src/main/java/com/shadowos/agrifarm/shadowact2.java
@@ -8,19 +8,28 @@ import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class shadowact2 extends AppCompatActivity {
     private WebView webView;
+    private ImageView viewv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(com.shadowos.agrifarm.R.layout.activity_shadowact2);
+        ImageView imageView = (ImageView) findViewById(R.id.imageView3);
+        Glide.with(this).asGif().load(R.drawable.giphy3).into(imageView);
         webView = (WebView) findViewById(R.id.wewewew);
         webView.setWebViewClient(new WebViewClient());
         webView.loadUrl("https://www.covid19india.org/");
         WebSettings webSettings = webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
+
+        ImageView viewv = (ImageView) findViewById(R.id.imageView6);
+        Glide.with(this).asGif().load(R.drawable.sourcecode2).into(viewv);
 
     }
 
@@ -42,7 +51,16 @@ public class shadowact2 extends AppCompatActivity {
         startActivity(intent);
 
     }
+
     public void seedinfo(View view) {
         Intent intent = new Intent(this, shadowact7.class);
         startActivity(intent);
-}}
+    }
+
+    public void sourcecode(View view) {
+        Intent intent = new Intent(this, shadowact8.class);
+        startActivity(intent);
+
+
+    }
+}

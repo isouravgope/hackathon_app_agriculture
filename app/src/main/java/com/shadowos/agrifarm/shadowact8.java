f4a2f9d9175e0abb6d698ed9e7770b387e1714cd
theglitchh <souravgope765@gmail.com>
2020-05-23 02:21:42 +0530

    Agrifarm: Add new ImageView for github animated logo
    -TODO set to string:large_text
    -drawable: Add raw gifs for activities

--- app/src/main/java/com/shadowos/agrifarm/shadowact8.java
+++ app/src/main/java/com/shadowos/agrifarm/shadowact8.java
@@ -1,13 +1,32 @@
 package com.shadowos.agrifarm;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
 
 public class shadowact8 extends AppCompatActivity {
+    private ImageView ViewClass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shadowact8);
+        WebView myWebView = (WebView) findViewById(R.id.sourceweb);
+        myWebView.loadUrl("https://github.com/Newbie-app-Developers/hackathon_app_agriculture.git");
+
+        ImageView ViewClass = (ImageView) findViewById(R.id.imageView7);
+        Glide.with(this).asGif().load(R.drawable.thankyou).into(ViewClass);
+
     }
+    public void BackToMenu(View view) {
+        Intent intent = new Intent(this, shadowact2.class);
+        startActivity(intent);
+}
 }
